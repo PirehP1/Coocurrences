afafda002091502c9128c8feaa33a1c01ba411f5
SLamasse <74814681+SLamasse@users.noreply.github.com>
2022-01-06 11:50:43 +0100

    Add files via upload

--- traitement/traitement.py
+++ traitement/traitement.py
@@ -17,7 +17,6 @@ __authors__ = "Audrey Quessada Vial","Stephane Lamassé"
 import sys
 sys.path.append("../")
 import re
-import itertools
 import os
 import functools
 import time
@@ -79,12 +78,12 @@ class Preprocessing:
             list_file = []
             for root, dirs, files in os.walk(self.input_path, topdown=True):
                 for name in files:
-                    if name.endswith(ext):
-                        list_file.append(os.path.join(root, name))
+
+                    list_file.append(os.path.join(root, name))
             return list_file
 
                             
-    def get_files_bigvolume(self, ext="txt"):
+    def get_files_bigvolume(self):
         """
         
         This function retrieves files paths from a root folder when the number of files is important
@@ -101,8 +100,8 @@ class Preprocessing:
         print("= traitement d'un gros volume de fichiers")
         for root, dirs, files in os.walk(self.input_path, topdown=True):
             for name in files:
-                if name.endswith(ext):
-                    yield os.path.join(root, name)                
+                #if name.endswith(ext):
+                yield os.path.join(root, name)                
         
         
     #@timer    
@@ -139,6 +138,8 @@ class Preprocessing:
         if remove_punctuation == True:
             text = re.sub(regex_punctuation, " ", text)
         text = re.sub(regex_clean, "", text)
+        text = re.sub(r"\d*", "", text)
+        text = re.sub(r'"', "", text)
         text = re.sub("(\s{2,})", " ", text)
         return text
     
@@ -181,8 +182,10 @@ class Preprocessing:
                 list_token = []
                 for token in doc:
                     token = re.sub("(\s{2})?", "", token)
-                    if (token not in stopwords): #on filtre les mots qui ne sont pas des stop words et dont la longueur est > 2
-                        if len(token) >= filter_by_length:
+                    for token in doc:
+                        if (token in stopwords) or (len(token)<= filter_by_length) or (token==""):
+                            pass
+                        else: #on filtre les mots qui ne sont pas des stop words et dont la longueur est > 2
                             list_token.append(token)
                 if list_token != []:
                     if joint == True:
@@ -226,9 +229,11 @@ class Preprocessing:
             if doc != []:
                 for token in doc:
                     token = re.sub("(\s{2})?", "", token) #on enlève les espaces supplémentaires quand il y en a
-                    if token not in stopwords: #on filtre les mots qui ne sont pas des stop words et dont la longueur est > 2
-                        if len(token) >= filter_by_length:
-                            yield token
+                    if (token in stopwords) or (len(token)<= filter_by_length) or (token==""):
+                            pass
+                    else: #on filtre les mots qui ne sont pas des stop words et dont la longueur est > 2
+                        #print(token)
+                        yield token
 
 
     #@timer
@@ -253,17 +258,21 @@ class Preprocessing:
         """
 
         print("=> reading full text")
+        remove_punctuation = kwargs.get("remove_puntuation", self.remove_punctuation)
+        regex_punctuation = kwargs.get("regex_punctuation", "[',;:!\\/.?{}\|\-_§&#()\[\]@<>\+\-\*%]")
+        case = kwargs.get("case", self.case)
+        encoding = kwargs.get("encoding", "utf-8")
+        start = kwargs.get("start",0)
         if file is not None:
-            with open(file, "r") as f:
+            with open(file, "r", encoding=encoding) as f:
+                f.seek(start)
                 doc = f.read()
         else:
             doc = kwargs.get("texte", None)
         if doc is None:
             print("empty file or text string")
             sys.exit()
-        remove_punctuation = kwargs.get("remove_puntuation", self.remove_punctuation)
-        regex_punctuation = kwargs.get("regex_punctuation", "[',;:!\\/.?{}\|\-_§&#()\[\]@<>\+\-\*%]")
-        case = kwargs.get("case", self.case)
+
         return [self.basic_text_cleaning(doc, 
                                          regex_clean, 
                                          case=case,
@@ -302,8 +311,11 @@ class Preprocessing:
         regex_punctuation = kwargs.get("regex_punctuation", "[',;:!\\/.?{}\|\-_§&#()\[\]@<>\+\-\*%]")
         case = kwargs.get("case", self.case)
         punct = kwargs.get("punct", ".")
+        encoding = kwargs.get("encoding", "utf-8")
+        start = kwargs.get("start",0) #pour commencer à lire un texte à un numéro de caractère donné
         if file is not None:
-            with open(file, "r") as f:
+            with open(file, "r", encoding=encoding) as f:
+                f.seek(start)
                 doc = f.read()
         else:
             doc = kwargs.get("texte", None) 
@@ -353,8 +365,11 @@ class Preprocessing:
         """
 
         print("= reading by window")
+        encoding = kwargs.get("encoding", "utf-8")
+        start = kwargs.get("start", 0)
         if file is not None:
-            with open(file, "r") as f:
+            with open(file, "r", encoding=encoding) as f:
+                f.seek(start)
                 doc = f.read()
         else:
             doc = kwargs.get("texte", None)
@@ -428,8 +443,11 @@ class Preprocessing:
         """
 
         print("= reading by sliding window")
+        encoding = kwargs.get("encoding", "utf-8")
+        start = kwargs.get("start", 0)
         if file is not None:
-            with open(file, "r") as f:
+            with open(file, "r", encoding=encoding) as f:
+                f.seek(start)
                 doc = f.read()
         else:
             doc = kwargs.get("texte", None)
@@ -456,7 +474,7 @@ class Preprocessing:
         if len(list_words) < window:
             print("text size lower than window size, please select another window size")
             sys.exit()
-        list_window = []
+        #list_window = []
         for i in range(0, len(list_words), step):
             chunk = " ".join(list_words[i:i+window+1])
             yield chunk
@@ -482,150 +500,6 @@ def read_stop_word(path):
         stop_words = f.readlines()
     f.close()
     return [words.strip() for words in stop_words]
-                  
-                        
-                    
-                
-if __name__ == "__main__":
-    
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--input_path', default=None, help="folder path of the text files, str")
-    parser.add_argument('--path_stop', default=None, help="path of stop words list")
-    parser.add_argument('--stopwords', default=None, help="list of stop words")
-    parser.add_argument("--window", default=5, type=int, help="analysis window, nb of words") 
-    parser.add_argument("--remove_punctuation", default=True, type=bool, help="boolean to know if we want to remove all the punctuation")
-    parser.add_argument("--filter_by_length", default=0, type=int, help="value to filter words by their length")
-    parser.add_argument("--separator", default=" ", type=str, help="simple separator to get tokens from text")
-    parser.add_argument("--case", default=True, type=bool, help="boolean to know if we want to lower cases")
-    args = parser.parse_args()
-    if args.input_path == None:
-        input_path = "../Data/corpusbytime"
-    else:
-        input_path = args.input_path
-    if isinstance(args.stopwords, list):
-        stop_words = args.stopwords
-    else:
-        if args.path_stop == None and args.stopwords==None:
-            stop_words = []
-        else:
-            path_stop_words = "../Data/stop_words.txt"
-            stop_words = read_stop_word(path_stop_words)
-    #print(stop_words)
-    window = args.window
-    remove_punctuation = args.remove_punctuation
-    filter_by_length = args.filter_by_length
-    separator = args.separator
-    case = args.case
-    preproc = Preprocessing(input_path=input_path,
-                            window=window,
-                            stop_words=stop_words,
-                            remove_punctuation=remove_punctuation,
-                            filter_by_length=filter_by_length,
-                            separator=separator,
-                            case=case)
-    
-    print(r"....TEST FONCTION GET_FILES")
-    list_file = preproc.get_files_simple()
-    print(list_file)
-    gen_file = preproc.get_files_bigvolume()
-    print(next(gen_file))
-    #definition d'une regex
-    regex_clean = "(\')?(\d*)(\\n)"
-    print("----------------")
-    print(r"....1.TEST FONCTION BASIC_TEST_CLEANING AVEC PONCTUATION")
-    text_basic = """Le but de toute association politique est la conservation des droits naturels et imprescriptibles de l'Homme. 
-    Ces droits sont la liberté, la propriété, la sûreté, et la résistance à l'oppression.<>?;:!/§+-*&@{}[|]()"""
-    text_basic1 = preproc.basic_text_cleaning(text_basic, regex_clean, remove_punctuation=True)
-    print(text_basic1)
-    print("----------------")
-    print(" ")
-    print(r"....2.TEST FONCTION BASIC_TEST_CLEANING SANS PONCTUATION")
-    text_basic2 = preproc.basic_text_cleaning(text_basic, regex_clean, remove_punctuation=False)
-    print(text_basic2)
-    print("----------------")
-    print(" ")
-    print(r"....3.TEST FONCTION READ_TEXT_FULL SANS KWARGS")
-    doc = preproc.read_text_full(list_file[0], regex_clean)
-    print(doc[0][:100])
-    print("----------------")
-    print(" ")
-    print(r"....4.TEST FONCTION READ_TEXT_FULL AVEC KWARGS")
-    doc1 = preproc.read_text_full(None, regex_clean, texte=text_basic)
-    print(doc1)
-    print("----------------")
-    print(" ")
-    print(r"....5.TEST FONCTION GET_TOKEN AVEC FULL TEXT")
-    gen_tok_full = preproc.get_token(doc1, filter_by_length=2) #filter_by_length can be  0
-    print(list(gen_tok_full))
-    print("----------------")
-    print(" ")
-    print(r"....6.TEST FONCTION READ_TEXT_SENTENCE SANS KWARGS")
-    gen_sent = preproc.read_text_sentence(list_file[0], regex_clean)
-    print(next(gen_sent))
-    print("----------------")
-    print(" ")
-    print(r"....7.TEST FONCTION READ_TEXT_SENTENCE AVEC KWARGS")
-    gen_sent1 = preproc.read_text_sentence(None, regex_clean, texte=text_basic)
-    print(next(gen_sent1))
-    print("----------------")
-    print(" ")
-    print(r"....8.TEST FONCTION GET_TOKEN POUR SENTENCE AVEC KWARGS")
-    gen_sent1 = preproc.read_text_sentence(None, regex_clean, texte=text_basic)
-    tok_sent1 = preproc.get_token(gen_sent1)
-    for tok in tok_sent1:
-        print(tok)
-    print("----------------")
-    print(" ")
-    print(r"....9.TEST FONCTION GET_TOKEN_WINDOW AVEC SENTENCE")
-    gen_sent1 = preproc.read_text_sentence(None, regex_clean, texte=text_basic)
-    tok_sent = preproc.get_token_window(gen_sent1, filter_by_length=2)
-    for tok in tok_sent:
-        print(tok)
-    print("----------------")
-    print(" ")
-    print(r"....10.TEST FONCTION READ_TEXT_WINDOW SANS KWARGS")
-    gen_wind = preproc.read_text_window(list_file[0], regex_clean)
-    print(next(gen_wind))
-    print("----------------")
-    print(" ")
-    print(r"....11.TEST FONCTION READ_TEXT_WINDOW AVEC KWARGS")
-    gen_wind1 = preproc.read_text_window(None, regex_clean, texte=text_basic)
-    print(next(gen_wind1))
-    print("----------------")
-    print(" ")
-    print(r"....12.TEST FONCTION GET_TOKEN_WINDOW AVEC WINDOW")
-    tok_wind = preproc.get_token_window(gen_wind1, filter_by_length=2)
-    print(next(tok_wind))
-    print("----------------")
-    print(" ")
-    print(r"....13.TEST FONCTION READ_TEXT_SLIDING_WINDOW SANS KWARGS")
-    gen_slwind = preproc.read_text_sliding_window(list_file[0], regex_clean, step=1)
-    print(next(gen_slwind))
-    print("----------------")
-    print(" ")
-    print(r"....14.TEST FONCTION READ_TEXT_SLIDING_WINDOW AVEC KWARGS")
-    gen_slwind1 = preproc.read_text_sliding_window(None, regex_clean, step=2, texte=text_basic)
-    for gen in gen_slwind1:
-        print(gen)
-    tok_slwind1 = preproc.get_token_window(gen_slwind1, filter_by_length=2, joint=True)
-    for tok in tok_slwind1:
-        print(tok)
-    print("----------------")
-    print(" ")
-    print(r"....15.TEST FONCTION GET_TOKEN AVEC WINDOW")
-    gen_wind3 = preproc.read_text_window(None, regex_clean, texte=text_basic)
-    tok_sent3 = preproc.get_token(gen_wind3)
-    for tok in tok_sent3:
-        print(tok)
-    print("----------------")
-    print(" ")
-    print(r"....16.TEST FONCTION GET_TOKEN_WINDOW AVEC FULL_TEXT")
-    tok_slwind2 = preproc.get_token_window(doc, filter_by_length=2, joint=False)
-    for tok in tok_slwind2:
-        print(tok)
-    
-       
-        
-    
-            
 
+                        
+                 
